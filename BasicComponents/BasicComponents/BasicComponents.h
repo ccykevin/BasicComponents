0e7636c6a800b0d248b0606d02e646c648911ed7
Kevin Cheng <chunyinkevin@gmail.com>
2018-04-20 16:37:07 +0800

    Create and setup empty Cocoa Touch Framework Project. Add git ignore file.

---
+++ BasicComponents/BasicComponents/BasicComponents.h
@@ -0,0 +1,19 @@
+//
+//  BasicComponents.h
+//  BasicComponents
+//
+//  Created by Kevin Cheng on 20/4/2018.
+//  Copyright © 2018 Kevin Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicComponents.
+FOUNDATION_EXPORT double BasicComponentsVersionNumber;
+
+//! Project version string for BasicComponents.
+FOUNDATION_EXPORT const unsigned char BasicComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicComponents/PublicHeader.h>
+
+
